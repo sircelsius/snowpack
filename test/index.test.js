c0058b657a0bb384d3e7ad882fe64ce5e3d50b9f
Marc du Boucheron <bramauddub@eisti.eu>
2016-07-06 20:54:17 +0200

    SPCK-0 travis

---
+++ test/index.test.js
@@ -0,0 +1,9 @@
+import chai from 'chai';
+var should = require('chai').should();
+
+describe('Tests', () => {
+    it('should run', () => {
+      true.should.be.true
+    });
+});
+
--- tests.webpack.js
+++ tests.webpack.js
@@ -4,6 +4,6 @@ const testsContext = require.context('./src/lib/', true, /\.js$/);
 testsContext.keys().forEach(testsContext);
 
 // require all `src/components/**/index.js`
-const componentsContext = require.context('./src/test/', true, /test\.js$/);
+const componentsContext = require.context('./test/', true, /test\.js$/);
 
 componentsContext.keys().forEach(componentsContext);
